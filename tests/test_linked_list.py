7dc769760c20ce689816ae35e7e1fd914ce5e276
u_alex90 <u_alex90@mail.ru>
2023-08-05 10:26:36 +1000

    mod test_linkedlist

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -3,8 +3,13 @@ from src.linked_list import LinkedList
 
 
 def test_linkedlist(ll_1, ll_2):
+    # Test case #1
     assert str(ll_1) == "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
     assert str(ll_2) == "{'id': 0} -> {'id': 1} -> {'id': 2} -> None"
+    # Test case #2
+    assert ll_1.head.data == {'id': 0}
+    assert ll_1.head.next_node.data == {'id': 1}
+    assert ll_1.tail.next_node is None
+    # Test case #3
     ll_3 = LinkedList()
     assert str(ll_3) == "None"
-    
